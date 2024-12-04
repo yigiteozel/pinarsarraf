c7a145bbbbc146a618f2af14b40f21ead8e0bb2b
yigiteozel <yigitcan.akgunlu1@gmail.com>
2024-12-04 18:49:02 +0300

    pinarsarraf.py

--- pinarsarraf.py
+++ pinarsarraf.py
@@ -14,19 +14,13 @@ for attribute in dir(SendSms):
 while 1:
     system("cls||clear")
     print("""{}
-     ______                         _     
-    |  ____|                       | |    
-    | |__   _ __   ___  _   _  __ _| |__  
-    |  __| | '_ \ / _ \| | | |/ _` | '_ \ 
-    | |____| | | | (_) | |_| | (_| | | | |
-    |______|_| |_|\___/ \__,_|\__, |_| |_|
-                               __/ |      
-                              |___/      
+     PINAR SARRAFA ÖZEL TASARLANMIŞTIR.
+     KODLAMASI DEĞİŞTİRİLEMEZ.
     
-    Sms: {}           {}by {}@tingirifistik\n  
+    Sms: {}           {}by {}@p1narss0\n  
     """.format(Fore.LIGHTCYAN_EX, len(servisler_sms), Style.RESET_ALL, Fore.LIGHTRED_EX))
     try:
-        menu = (input(Fore.LIGHTMAGENTA_EX + " 1- SMS Gönder\n\n 2- Çıkış\n\n" + Fore.LIGHTYELLOW_EX + " Seçim: "))
+        menu = (input(Fore.LIGHTMAGENTA_EX + " 1- SMS Gönder\n\n 2- Çıkış\n\n" + Fore.LIGHTYELLOW_EX + " Seçimini yap pınar hanım: "))
         if menu == "":
             continue
         menu = int(menu) 
